09a9260eabb41c4e09fc77218f1d3e6d8b921c8e
Ross Schlie <ross.c.schlie@gmail.com>
2021-01-07 07:26:16 -0800

    Update high scores with more pthon-ish list syntax usage

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -7,23 +7,14 @@
 # Your task is to write methods that return the highest score from the list, the last added score and the three highest scores.
 
 def latest(scores):
-    return scores[len(scores) - 1]
+    return scores[- 1]
 
 def personal_best(scores):
-    scores.sort(reverse=True)
-    return scores[0]
+    return max(scores)
 
 def personal_top_three(scores):
     scores.sort(reverse=True)
-    topc = 3
-    top = []
-    if len(scores) < topc:
-        topc = len(scores)
-
-    for i in range(0, topc):
-        top.append(scores[i])
-
-    return top
+    return scores[:3]
 
 # Test code
 # lscore = [100, 0, 90, 30]
