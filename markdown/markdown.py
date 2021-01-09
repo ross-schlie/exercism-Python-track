0b25f9535a7c50245780871614cc03c3433948f1
Ross Schlie <ross.c.schlie@gmail.com>
2021-01-09 15:46:48 -0800

    Base state of markdown (prior to refactoring)

---
+++ markdown/markdown.py
@@ -0,0 +1,83 @@
+'''
+Introduction
+Refactor a Markdown parser.
+
+The markdown exercise is a refactoring exercise. 
+There is code that parses a given string with Markdown syntax and returns the associated HTML for that string. 
+Even though this code is confusingly written and hard to follow, somehow it works and all the tests are passing! 
+Your challenge is to re-write this code to make it easier to read and maintain while still making sure that all the tests keep passing.
+
+It would be helpful if you made notes of what you did in your refactoring in comments so reviewers can see that, 
+but it isn't strictly necessary. The most important thing is to make the code better!
+'''
+
+import re
+
+def parse(markdown):
+    lines = markdown.split('\n')
+    res = ''
+    in_list = False
+    in_list_append = False
+    for i in lines:
+        if re.match('###### (.*)', i) is not None:
+            i = '<h6>' + i[7:] + '</h6>'
+        elif re.match('## (.*)', i) is not None:
+            i = '<h2>' + i[3:] + '</h2>'
+        elif re.match('# (.*)', i) is not None:
+            i = '<h1>' + i[2:] + '</h1>'
+        m = re.match(r'\* (.*)', i)
+        if m:
+            if not in_list:
+                in_list = True
+                is_bold = False
+                is_italic = False
+                curr = m.group(1)
+                m1 = re.match('(.*)__(.*)__(.*)', curr)
+                if m1:
+                    curr = m1.group(1) + '<strong>' + \
+                        m1.group(2) + '</strong>' + m1.group(3)
+                    is_bold = True
+                m1 = re.match('(.*)_(.*)_(.*)', curr)
+                if m1:
+                    curr = m1.group(1) + '<em>' + m1.group(2) + \
+                        '</em>' + m1.group(3)
+                    is_italic = True
+                i = '<ul><li>' + curr + '</li>'
+            else:
+                is_bold = False
+                is_italic = False
+                curr = m.group(1)
+                m1 = re.match('(.*)__(.*)__(.*)', curr)
+                if m1:
+                    is_bold = True
+                m1 = re.match('(.*)_(.*)_(.*)', curr)
+                if m1:
+                    is_italic = True
+                if is_bold:
+                    curr = m1.group(1) + '<strong>' + \
+                        m1.group(2) + '</strong>' + m1.group(3)
+                if is_italic:
+                    curr = m1.group(1) + '<em>' + m1.group(2) + \
+                        '</em>' + m1.group(3)
+                i = '<li>' + curr + '</li>'
+        else:
+            if in_list:
+                in_list_append = True
+                in_list = False
+
+        m = re.match('<h|<ul|<p|<li', i)
+        if not m:
+            i = '<p>' + i + '</p>'
+        m = re.match('(.*)__(.*)__(.*)', i)
+        if m:
+            i = m.group(1) + '<strong>' + m.group(2) + '</strong>' + m.group(3)
+        m = re.match('(.*)_(.*)_(.*)', i)
+        if m:
+            i = m.group(1) + '<em>' + m.group(2) + '</em>' + m.group(3)
+        if in_list_append:
+            i = '</ul>' + i
+            in_list_append = False
+        res += i
+    if in_list:
+        res += '</ul>'
+    return res
