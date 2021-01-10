3014e37551b83c46c55da51ff4fe0c4e94a81072
Ross Schlie <ross.c.schlie@gmail.com>
2021-01-09 16:18:11 -0800

    Minor refactor of markdown splitting up markup functions

--- markdown/markdown.py
+++ markdown/markdown.py
@@ -19,65 +19,97 @@ def parse(markdown):
     in_list = False
     in_list_append = False
     for i in lines:
-        if re.match('###### (.*)', i) is not None:
-            i = '<h6>' + i[7:] + '</h6>'
-        elif re.match('## (.*)', i) is not None:
-            i = '<h2>' + i[3:] + '</h2>'
-        elif re.match('# (.*)', i) is not None:
-            i = '<h1>' + i[2:] + '</h1>'
+        i = markup_heading(i)
         m = re.match(r'\* (.*)', i)
         if m:
+            curr = m.group(1)
+            curr = markup_strong(curr)
+            curr = markup_em(curr)
             if not in_list:
                 in_list = True
-                is_bold = False
-                is_italic = False
-                curr = m.group(1)
-                m1 = re.match('(.*)__(.*)__(.*)', curr)
-                if m1:
-                    curr = m1.group(1) + '<strong>' + \
-                        m1.group(2) + '</strong>' + m1.group(3)
-                    is_bold = True
-                m1 = re.match('(.*)_(.*)_(.*)', curr)
-                if m1:
-                    curr = m1.group(1) + '<em>' + m1.group(2) + \
-                        '</em>' + m1.group(3)
-                    is_italic = True
                 i = '<ul><li>' + curr + '</li>'
             else:
-                is_bold = False
-                is_italic = False
-                curr = m.group(1)
-                m1 = re.match('(.*)__(.*)__(.*)', curr)
-                if m1:
-                    is_bold = True
-                m1 = re.match('(.*)_(.*)_(.*)', curr)
-                if m1:
-                    is_italic = True
-                if is_bold:
-                    curr = m1.group(1) + '<strong>' + \
-                        m1.group(2) + '</strong>' + m1.group(3)
-                if is_italic:
-                    curr = m1.group(1) + '<em>' + m1.group(2) + \
-                        '</em>' + m1.group(3)
                 i = '<li>' + curr + '</li>'
         else:
             if in_list:
                 in_list_append = True
                 in_list = False
 
-        m = re.match('<h|<ul|<p|<li', i)
-        if not m:
-            i = '<p>' + i + '</p>'
-        m = re.match('(.*)__(.*)__(.*)', i)
-        if m:
-            i = m.group(1) + '<strong>' + m.group(2) + '</strong>' + m.group(3)
-        m = re.match('(.*)_(.*)_(.*)', i)
-        if m:
-            i = m.group(1) + '<em>' + m.group(2) + '</em>' + m.group(3)
+        i = markup_paragraph(i)
+        i = markup_strong(i)
+        i = markup_em(i)
         if in_list_append:
             i = '</ul>' + i
             in_list_append = False
+
         res += i
     if in_list:
         res += '</ul>'
     return res
+
+def markup_heading(text):
+    '''
+    Markup text in a headings <h6|h2|h1> when ###### found  
+  
+    Parameters: 
+    arg1 (text): the line of code being considered for markup 
+  
+    Returns: 
+    text: Marked up code with <h6|h2|h1>
+    '''
+    if re.match('###### (.*)', text) is not None:
+        text = '<h6>' + text[7:] + '</h6>'
+    elif re.match('## (.*)', text) is not None:
+        text = '<h2>' + text[3:] + '</h2>'
+    elif re.match('# (.*)', text) is not None:
+        text = '<h1>' + text[2:] + '</h1>'
+    return text
+
+def markup_paragraph(text):
+    '''
+    Markup text in a paragraph <p> when not in h/ul/p/li already  
+  
+    Parameters: 
+    arg1 (text): the line of code being considered for markup 
+  
+    Returns: 
+    text: Marked up code with <p>
+    '''
+    m = re.match('<h|<ul|<p|<li', text)
+    if not m:
+        text = '<p>' + text + '</p>'
+
+    return text
+
+def markup_strong(text):
+    '''
+    Markup text in a <strong> when (dobule) heavily underlined 
+  
+    Parameters: 
+    arg1 (text): the line of code being considered for markup 
+  
+    Returns: 
+    text: Marked up code with <strong>
+    '''
+    m = re.match('(.*)__(.*)__(.*)', text)
+    if m:
+        text = m.group(1) + '<strong>' + m.group(2) + '</strong>' + m.group(3)
+    return text
+
+def markup_em(text):
+    '''
+     Markup text in a <em> when underlined 
+  
+    Parameters: 
+    arg1 (text): the line of code being considered for markup 
+  
+    Returns: 
+    text: Marked up code with <em>
+    '''
+    m = re.match('(.*)_(.*)_(.*)', text)
+    if m:
+        text = m.group(1) + '<em>' + m.group(2) + '</em>' + m.group(3)
+    return text
+
+# a = parse("This will be a paragraph")
+# print(a) # "<p>This will be a paragraph</p>"
