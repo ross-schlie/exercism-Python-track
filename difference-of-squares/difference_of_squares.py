400d41d1bfdab9c05d239103fa9edd29e805ecf9
Ross Schlie <ross.c.schlie@gmail.com>
2021-10-27 21:28:38 -0700

    difference of squares inefficient solution.

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,10 +1,21 @@
+"""exercism difference of squares module."""
+
+
 def square_of_sum(number):
-    pass
+    number_sum = 0
+    for number in range(1, number + 1):
+        number_sum += number
+
+    return number_sum ** 2
 
 
 def sum_of_squares(number):
-    pass
+    squares_sum = 0
+    for number in range(1, number + 1):
+        squares_sum += number ** 2
+
+    return squares_sum
 
 
 def difference_of_squares(number):
-    pass
+    return square_of_sum(number) - sum_of_squares(number)
